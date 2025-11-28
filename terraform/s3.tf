28231b4d0487215a3a567a401a90d0d01026afd5
Peter Chege <emchegep@gmail.com>
2025-11-28 17:08:40 +0300

    added s3 bucket versioning and blocked public access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -33,5 +33,30 @@ resource "aws_s3_bucket_policy" "portfolio" {
   policy = data.aws_iam_policy_document.origin_bucket_policy.json
 }
 
+resource "aws_s3_bucket_versioning" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio" {
+  bucket                  = aws_s3_bucket.portfolio.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 
 
