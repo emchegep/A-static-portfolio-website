ffa6a903559a70c4ce26ef0598c075052348efb3
Peter Chege <emchegep@gmail.com>
2025-11-28 12:43:51 +0300

    added input variables

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type        = string                     # The type of the variable, in this case a string
+  default     = "us-east-2"              # Default value for the variable
+  description = "AWS default region" # Description of what this variable represents
+}
